e7d40ed3e3d9dcedbaa6be1c81782deb81ee5a4a
Davidrums <david@soff.nl>
2014-01-26 16:53:04 +0100

    add input fields

--- client/chat.js
+++ client/chat.js
@@ -1,11 +1,18 @@
+Messages = new Meteor.Collection("messages");
+Rooms = new Meteor.Collection("rooms");
+
+Template.messages.messages = function() {
+	return Messages.find().fetch();
+}
+
 Template.hello.greeting = function () {
     return "Welcome to chat.";
-  };
+};
 
-  Template.hello.events({
+Template.hello.events({
     'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+	    console.log("You pressed the button");
     }
-  });
+});
